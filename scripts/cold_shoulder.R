f54cf0af5126765438364168ba74caba235f3780
Kisei Tanaka <ktanaka@mbayaq.org>
2020-05-11 15:52:02 -0700

    oush

--- scripts/cold_shoulder.R
+++ scripts/cold_shoulder.R
@@ -1,3 +1,5 @@
+rm(list = ls())
+
 library(dplyr)
 library(ggplot2)
 library(raster)
@@ -5,7 +7,7 @@ library(doParallel)
 # cores = detectCores()/2
 # registerDoParallel(cores = cores)
 
-load("/Users/ktanaka/Dropbox (MBA)/Data/oisst/depth_0.25.Rdata")
+load("/Users/ktanaka/jws_range/data/depth_0.25.Rdata")
 load("/Users/Kisei/jws_range/data/depth_0.25.Rdata")
 
 depth = d
@@ -16,7 +18,7 @@ r = foreach(year = 1981:2020, .combine = rbind) %dopar% {
   
   # year = 2019
   
-  load(paste0("/Users/Kisei/jws_range/data/sst.day.mean.", year , ".RData"))
+  load(paste0("/Users/ktanaka/jws_range/data/sst.day.mean.", year , ".RData"))
   
   year_sum = NULL
   
@@ -31,15 +33,15 @@ r = foreach(year = 1981:2020, .combine = rbind) %dopar% {
     d = rasterToPoints(d)
     d = as.data.frame(d)
     colnames(d) = c("x", "y", "z")
-    # d = merge(d, depth)
+    d = merge(d, depth, all = T)
     d$z = ifelse(d$z < 18.1 & d$z > 17.9, 1, 0)
     d$time = time
     
-    d %>%
-      ggplot(aes(x, y, color = z)) +
-      geom_point() +
-      scale_color_viridis_c() +
-      geom_smooth(data = subset(d, z > 0.5))
+    # d %>%
+    #   ggplot(aes(x, y, color = z)) +
+    #   geom_point() +
+    #   scale_color_viridis_c() +
+    #   geom_smooth(data = subset(d, z > 0.5))
 
     year_sum = rbind(year_sum, d)
     
@@ -52,9 +54,9 @@ r = foreach(year = 1981:2020, .combine = rbind) %dopar% {
 }
 
 df = as.data.frame(r)
-save(df, file = paste0("/Users/ktanaka/Dropbox (MBA)/Data/oisst/shark_coldline_", Sys.Date(), ".Rdata"))
+save(df, file = paste0("/Users/ktanaka/jws_range/results/cold_shoulder_", Sys.Date(), ".Rdata"))
 
-load('/Users/Kisei/jws_range/results/shark_coldline_2020-05-08.Rdata')
+load('/Users/Kisei/jws_range/results/cold_shoulder_2020-05-08.Rdata')
 
 df$month = substr(as.character(df$time), 6, 7)
 df = subset(df, month %in% c("06", "07", "08", "09", "10"))
