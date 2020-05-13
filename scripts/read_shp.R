21a34d251104fc17f7860e5f9f2a2c92c7bdfd30
krtanaka <kisei.tanaka@gmail.com>
2020-05-13 14:13:36 -0700

    sdfd

--- scripts/read_shp.R
+++ scripts/read_shp.R
@@ -3,17 +3,19 @@ library(sf)
 shp <- list.files('/Users/ktanaka/Dropbox (MBA)/PAPER Climate refugia/CR_R_scripts/CR_shapefiles/s_11au16/', pattern = '\\.shp$') # list all files in the MERRA model folder
 
 
-s <- st_read('/Users/ktanaka/Dropbox (MBA)/PAPER Climate refugia/CR_R_scripts/CR_shapefiles/bathypol.shp')
+s <- st_read('/Users/kisei/Desktop/CR_shapefiles/CR_shapefiles/bathypol.shp')
 ggplot() + 
   geom_sf(data = s, color = "black", fill = "cyan1") + 
   coord_sf()
 
-s <- st_read('/Users/ktanaka/Dropbox (MBA)/PAPER Climate refugia/CR_R_scripts/CR_shapefiles/Bathypol100km.shp')
+s <- st_read('/Users/Kisei/Desktop/CR_shapefiles/CR_shapefiles/Bathypol1000_10km.shp')
 ggplot() + 
   geom_sf(data = s %>% sample_frac(1), fill = "cyan1") + 
   coord_sf()
 
-s <- st_read('/Users/ktanaka/Dropbox (MBA)/PAPER Climate refugia/CR_R_scripts/CR_shapefiles/Shp/Bathypol_new_poly.shp')
+s
+
+s <- st_read('/Users/Kisei/Desktop/CR_shapefiles/CR_shapefiles/Bathypol_new_poly.shp')
 ggplot() + 
   geom_sf(data = s, color = "black", fill = "cyan1") + 
   coord_sf()
