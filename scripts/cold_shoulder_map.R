108af477288c992e4f11f5897f1a553db88a55f5
krtanaka <kisei.tanaka@gmail.com>
2020-05-16 10:40:50 -0700

    df

--- scripts/cold_shoulder_map.R
+++ scripts/cold_shoulder_map.R
@@ -7,7 +7,7 @@ library(raster)
 setwd("/Users/ktanaka/Dropbox (MBA)/PAPER Kisei Bia JWS range shift/data/")
 setwd("/Users/Kisei/Dropbox/PAPER Kisei Bia JWS range shift/data/")
 
-load(paste0('cold_shoulder_', Sys.Date(), '.Rdata'))
+load("cold_shoulder_2020-05-14.Rdata")
 
 # d = df %>% sample_frac(0.01)
 d = df; rm(df)
@@ -18,41 +18,46 @@ d = subset(d, month %in% c("06", "07", "08", "09", "10"
                              ))
 table(d$month)
 
+setwd("/Users/ktanaka/Dropbox (MBA)/PAPER Kisei Bia JWS range shift/figures/")
+setwd("/Users/Kisei/Dropbox/PAPER Kisei Bia JWS range shift/figures/")
+
 map = d %>% 
-  # group_by(x, y) %>%
   subset(year %in% c(1982:2019)) %>% 
-  
   mutate(period = case_when(year %in% c(1982:2013) ~ "1982-2013",
                             year %in% c(2014:2015) ~ "2014-2015",
                             year %in% c(2016:2019) ~ "2016-2019")) %>% 
-  # mutate(period = ifelse(year %in% c(1982:2013), "1982-2013", "2014-2019")) %>% 
   group_by(x, y, period) %>%
   summarise(z = mean(z))
 
 m = map %>% 
   ggplot(aes(x, y)) + 
-  geom_tile(aes(fill = z), alpha = 0.9) + 
+  geom_tile(aes(fill = z), alpha = 1) + 
   geom_smooth(
     data = subset(map, z > 0.01), aes(color = period, group = period),
     method = "gam",
-    # method = "loess",
-    # method = "auto",
     span = 0.1,
     se = T) + 
-  scale_color_viridis_d("") +
+  geom_smooth(
+    data = subset(map, z > 0.01), aes(color = period, group = period),
+    method = "loess",
+    span = 0.1,
+    se = T) + 
+  geom_smooth(
+    data = subset(map, z > 0.01), aes(color = period, group = period),
+    method = "auto",
+    span = 0.1,
+    se = T) + 
+  scale_color_discrete("") +
   scale_fill_viridis_c("") + 
   borders(fill = "gray20") +
-  coord_quickmap(xlim = c(-127, -115),
-                 ylim = c(30, 45)) + 
-  # facet_wrap(.~ period) +
+  # coord_quickmap(xlim = c(-127, -115),
+  #                ylim = c(30, 45)) +
+  coord_quickmap(xlim = range(map$x),
+                 ylim = range(map$y)) +
+  facet_wrap(.~ period, scales = "fixed") +
   xlab("Longitude") + ylab("Latitude")
 
-m
-
-setwd("/Users/ktanaka/Dropbox (MBA)/PAPER Kisei Bia JWS range shift/figures/")
-setwd("/Users/Kisei/Dropbox/PAPER Kisei Bia JWS range shift/figures/")
-
-pdf('cold_shoulder_1.pdf', height = 6, width = 6)
+pdf('cold_shoulder_0.pdf', height = 6, width = 10)
 print(m)
 dev.off()
 
@@ -62,40 +67,41 @@ map = d %>%
   summarise(z = mean(z))
 
 m = map %>% 
-  subset(year %in% c(1982:2019)) %>%
-  ggplot(aes(x, y)) +  
-  # geom_tile(aes(fill = z), alpha = 0.9) +
-  geom_smooth(
-    data = subset(map, z > 0.01),
-    # method = "gam",
-    # method = "auto",
-    method = "loess",
-    span = 0.1,
-    aes(color = year, group = year), 
-    se = F) + 
-  scale_color_viridis_c("") +
+  subset(year %in% c(1982:2019)) %>% 
+  ggplot(aes(x, y)) + 
+  geom_raster(aes(fill = z), alpha = 0.9) +
+  # geom_smooth(
+  #   data = subset(map, z > 0.01),
+  #   method = "gam",
+  #   # method = "loess",
+  #   # method = "auto",
+  #   span = 0.1,
+  #   se = T) +
+  scale_color_viridis_d("") +
   scale_fill_viridis_c("") + 
   borders(fill = "gray20") +
-  coord_quickmap(xlim = c(-127, -115),
-                 ylim = c(30, 45)) + 
+  # coord_quickmap(xlim = c(-127, -115),
+  #                ylim = c(30, 45)) +
+  coord_quickmap(xlim = range(map$x),
+                 ylim = range(map$y)) + 
+  facet_wrap(.~ year, ncol = 10) +
+  theme_minimal() + 
   xlab("Longitude") + ylab("Latitude")
 
 m
 
-pdf('cold_shoulder_2.pdf', height = 6, width = 6)
+pdf('cold_shoulder_2.pdf', height = 6, width = 18)
 print(m)
 dev.off()
 
+
 lat = d %>% 
-  group_by(year) %>% 
   subset(z > 0 & depth > -1000) %>%
   # subset(z > 0) %>%
-  # summarise(z = mean(z)) %>%
+  group_by(year) %>% 
   summarise(y_mean = sum(z*y)/sum(z))
 
-m = map %>% 
-  subset(z > 0) %>%
-  subset(year %in% c(1982:2019)) %>%
+map %>% 
   ggplot(aes(x, y)) +  
   geom_hline(data = subset(lat, year %in% c(1982:2019)), 
              aes(yintercept = y_mean, color = year), size = 2, alpha = 0.8) + 
@@ -106,17 +112,12 @@ m = map %>%
   xlab("Longitude") + ylab("Latitude") +
   theme_classic()
 
-m
-
-pdf('cold_shoulder_3.pdf', height = 6, width = 6)
-print(m)
-dev.off()
 
 #time series
 setwd("/Users/ktanaka/Dropbox (MBA)/PAPER Kisei Bia JWS range shift/data/")
 setwd("/Users/Kisei/Dropbox/PAPER Kisei Bia JWS range shift/data/")
 
-load(paste0('cold_shoulder_', Sys.Date(), '.Rdata'))
+load('cold_shoulder_2020-05-14.Rdata')
 
 df = df %>% subset(z > 0 & depth > -1000)
 df$year = substr(as.character(df$time), 1, 4)
@@ -133,39 +134,25 @@ t2 = t1 %>%
   group_by(year) %>% 
   summarise(coldline = mean(coldline))
 
-lat$time = as.Date(lat$time)
-
-year = lat %>% group_by(year) %>% summarise(coldline = mean(coldline))
-
-lat$coldline = ifelse(lat$month %in% c("06", "07", "08", "09", "10"), lat$coldline, NA)
+t3 = t1 %>% 
+  mutate(month = substr(as.character(time), 6, 7)) %>% 
+  mutate(coldline = ifelse(month %in% c("06", "07", "08", "09", "10"), coldline, NA))
 
 library(zoo)
+pdf('cold_shoulder_time_series_1.pdf', height = 6, width = 8)
 
-pdf('cold_shoulder_time_series_1.pdf', height = 6, width = 10)
-pdf('cold_shoulder_time_series_2.pdf', height = 5, width = 10)
-
-ggplot(t1, aes(x = time, y = coldline, color = coldline)) +
-  geom_line(aes(y = rollmean(coldline, 1, na.pad = TRUE))) +
-  scale_color_viridis_c("") + 
-  # stat_smooth(method = "loess",
-  #             color = "gray60", 
-  #             fill = "gray60") +
-  ylab("JWS coldshoulder latitudinal mean") + 
-  xlab("") + 
-  ggtitle("10-day running mean") +
-  # ggtitle("10-day running mean, June-October") + 
-  # theme_minimal(I(20)) +
-  theme(legend.position = "none")
-
-ggplot(t2, aes(x = year, y = coldline, color = coldline)) +
-  geom_point() +
+ggplot(t3, aes(x = time, y = coldline, color = coldline)) +
+  geom_line(aes(y = rollmean(coldline, 10, na.pad = TRUE))) +
   scale_color_viridis_c("") + 
   stat_smooth(method = "loess",
+              span = 0.6,
               color = "gray60",
               fill = "gray60") +
-  ylab("11.55 deg C thermocline latitudinal mean") + 
+  ylab("JWS coldshoulder latitudinal mean") + 
   xlab("") + 
-  # theme_minimal(I(20)) +
+  # ggtitle("10-day running mean") +
+  ggtitle("10-day running mean, June-October") +
+  theme_classic() +
   theme(legend.position = "none")
 
 dev.off()
--- scripts/themal_histgram.R
+++ scripts/themal_histgram.R
@@ -13,8 +13,6 @@ load("/Users/ktanaka/Dropbox (MBA)/PAPER Kisei Bia JWS range shift/data/JWS_Corr
 
 # JWS_Corrected$Temperature = round(JWS_Corrected$Temperature, 1)
 
-# JWS_Corrected = subset(JWS_Corrected, id != c("JWS_14_07_12P0072_12398")) #remove Monterey Bay release
-
 d = JWS_Corrected
 d$month = substr(as.character(d$Time_s), 6, 7)
 plot(table(d$month))
@@ -23,86 +21,77 @@ d$count = 1
 d = d %>% group_by(lat_pop, lon_pop, id, sex) %>% summarise(count = sum(count))
 d$id = as.factor(d$id)
 
-setwd('/Users/Kisei/Dropbox/PAPER Kisei Bia JWS range shift/figures/')
+# setwd('/Users/Kisei/Dropbox/PAPER Kisei Bia JWS range shift/figures/supplement/')
+# pdf("tag_locations.pdf", height = 10, width = 8)
+# d %>% 
+#   ggplot(aes(lon_pop, lat_pop, 
+#              color = id,
+#              label = id)) + 
+#   borders(xlim = range(d$lon_pop),
+#           ylim = range(d$lat_pop),
+#           fill = "gray10") +
+#   coord_map(xlim = range(pretty(d$lon_pop)),
+#             ylim = range(pretty(d$lat_pop))) +
+#   geom_point() +
+#   ggrepel::geom_text_repel(aes(color = id), box.padding = 3) +
+#   xlab("Longitude") + ylab("Latitude") +
+#   # theme_classic2() + 
+#   theme(legend.position = "none")
+# dev.off()
+# 
+# pdf("tag_counts.pdf", height = 5, width = 6)
+# d %>% 
+#   ggplot(aes(lon_pop, lat_pop, 
+#              color = log10(count))) +
+#   borders(xlim = range(d$lon_pop),
+#           ylim = range(d$lat_pop),
+#           fill = "gray10") +
+#   coord_map(xlim = range(pretty(d$lon_pop)),
+#             ylim = range(pretty(d$lat_pop))) +
+#   geom_point(size = 5, alpha = 0.8) +
+#   xlab("Longitude") + ylab("Latitude") +
+#   # theme_classic2() + 
+#   scale_color_viridis_c("log10(n)") + 
+#   theme(legend.position = c(0.15, 0.2))
+# dev.off()
 
-pdf("tag_locations.pdf", height = 10, width = 8)
-d %>% 
-  ggplot(aes(lon_pop, lat_pop, 
-             color = id,
-             label = id)) + 
-  borders(xlim = range(d$lon_pop),
-          ylim = range(d$lat_pop),
-          fill = "gray10") +
-  coord_map(xlim = range(pretty(d$lon_pop)),
-            ylim = range(pretty(d$lat_pop))) +
-  geom_point() +
-  ggrepel::geom_text_repel(aes(color = id), box.padding = 3) +
-  xlab("Longitude") + ylab("Latitude") +
-  # theme_classic2() + 
-  theme(legend.position = "none")
+d1 = subset(JWS_Corrected, Depth <= 20); d1 = d1[,c("Temperature", "Depth", "id")]; d1$Depth_Range = "0-20m"; d1$count = 1
+d2 = subset(JWS_Corrected, Depth <= 2); d2 = d2[,c("Temperature", "Depth", "id")]; d2$Depth_Range = "0-2m"; d2$count = 1
+
+setwd("C:/Users/Kisei/Dropbox/PAPER Kisei Bia JWS range shift/figures/supplement/")
+
+pdf("thermal_profile_tag_raw.pdf", height = 5, width = 10)
+
+rbind(d1, d2) %>% 
+  sample_frac(1) %>%
+  ggplot(aes(x=Temperature, fill = Depth_Range, color = Depth_Range)) +
+  geom_density(alpha = 0.5) +
+  facet_wrap(.~id, scale = "free")
+
 dev.off()
 
-pdf("tag_counts.pdf", height = 5, width = 6)
-d %>% 
-  ggplot(aes(lon_pop, lat_pop, 
-             color = log10(count))) +
-  borders(xlim = range(d$lon_pop),
-          ylim = range(d$lat_pop),
-          fill = "gray10") +
-  coord_map(xlim = range(pretty(d$lon_pop)),
-            ylim = range(pretty(d$lat_pop))) +
-  geom_point(size = 5, alpha = 0.8) +
-  xlab("Longitude") + ylab("Latitude") +
-  # theme_classic2() + 
-  scale_color_viridis_c("log10(n)") + 
-  theme(legend.position = c(0.15, 0.2))
-dev.off()
+d1 = d1 %>%
+  group_by(id, Temperature) %>%
+  summarize(count = n()) %>%
+  mutate(count = range01(count),
+         Depth_Range = "0-20m")
 
-d1 = subset(JWS_Corrected, Depth <= 20); d1 = d1[,c("Temperature", "Depth", "id")]
-d2 = subset(JWS_Corrected, Depth <= 2); d2 = d2[,c("Temperature", "Depth", "id")]
+d2 = d2 %>%
+  group_by(id, Temperature) %>%
+  summarize(count = n()) %>%
+  mutate(count = range01(count),
+         Depth_Range = "0-2m")
 
-d1$count = 1
-d2$count = 1
-
-d1 = d1 %>% 
-  group_by(id, Temperature) %>% 
-  summarize(count = n()) %>% 
-  mutate(count = range01(count))
-
-d2 = d2 %>% 
-  group_by(id, Temperature) %>% 
-  summarize(count = n()) %>% 
-  mutate(count = range01(count))
-
-d1$Depth_Range = "0-20m"
-d2$Depth_Range = "0-2m"
-
-q_01 = aggregate(count ~ Temperature, data = d1, FUN = "sum")
-q1 = quantile(q_01$Temperature, probs = probs); order =  names(q1)
-q1 = as.data.frame(q1)
-q1$Depth = "0-20m"
-colnames(q1) = c("temp", "p")
-q1 = data.table::setDT(q1, keep.rownames = TRUE)[]
-
-q_02 = aggregate(count ~ Temperature, data = d2, FUN = "sum")
-q2 = quantile(q_02$Temperature, probs = probs)
-q2 = as.data.frame(q2)
-q2$Depth = "0-2m"
-colnames(q2) = c("temp", "p")
-q2 = data.table::setDT(q2, keep.rownames = TRUE)[]
-
-p = rbind(q1, q2)
-p
-
-d = rbind(d1, d2)
+d = rbind(d1, d2) 
 
 pdf("thermal_profile_tag.pdf", height = 5, width = 10)
 
 p = d %>% ggplot(aes(Temperature, count, color = Depth_Range, fill = Depth_Range)) + 
-  geom_bar(stat="identity", position = position_dodge(width = 0.5)) +
+  # geom_bar(stat="identity", position = position_dodge(width = 0.5)) +
+  geom_density(stat = "identity", alpha = 0.5) +
   facet_wrap(.~id) +
-  # scale_color_viridis_d() + 
-  # scale_fill_viridis_d() + 
+  # scale_color_viridis_d() +
+  # scale_fill_viridis_d() +
   # theme_minimal() + 
   ylab("Freq") + 
   theme(legend.position = "right")
@@ -133,17 +122,16 @@ t = rbind(t1, t2, t3)
 
 pdf("thermal_profile.pdf", height = 3, width = 10)
 
-p = t %>% 
+t %>% 
   ggplot(aes(x = Temperature, y = count, color = Depth_Range, fill = Depth_Range)) +
-  geom_bar(stat="identity", position = position_dodge(width = 0.5)) +  
-  scale_color_viridis_d() +
-  scale_fill_viridis_d() +
+  # geom_bar(stat="identity", position = position_dodge(width = 0.5)) +
+  geom_density(stat = "identity", alpha = 0.5) +
+  # scale_color_viridis_d() +
+  # scale_fill_viridis_d() +
   ylab("Freq") +  
   facet_wrap(.~ Bin_width, scales = "free", ncol = 3)+ 
   theme_cowplot() 
   
-p
-
 dev.off()
 
 occup = t %>% 
@@ -151,23 +139,15 @@ occup = t %>%
   mutate(sum = colSums(.[3]),
          count = count/sum)
 
+d = subset(occup, Bin_width == "0.5 deg C" & Depth_Range == "0-20m")
+
 library(mgcv)
-
-d = subset(occup, Bin_width == "0.1 deg C")
-
-g1 = gam(count ~ s(Temperature), 
-        data = d, family = "tw(theta = NULL, link = 'log', a = 1.01, b = 1.99)", gamma = 1.4)
-
-g2 = gam(count ~ s(Temperature, k = 5), 
-        data = d, family = "tw(theta = NULL, link = 'log', a = 1.01, b = 1.99)", gamma = 1.4)
-
-g3 = gam(count ~ s(Temperature, k = 3), 
-        data = d, family = "tw(theta = NULL, link = 'log', a = 1.01, b = 1.99)", gamma = 1.4)
-
+g1 = gam(count ~ s(Temperature), data = d, family = "tw(theta = NULL, link = 'log', a = 1.01, b = 1.99)", gamma = 1.4)
+g2 = gam(count ~ s(Temperature, k = 5), data = d, family = "tw(theta = NULL, link = 'log', a = 1.01, b = 1.99)", gamma = 1.4)
+g3 = gam(count ~ s(Temperature, k = 3), data = d, family = "tw(theta = NULL, link = 'log', a = 1.01, b = 1.99)", gamma = 1.4)
 d$pred_count_1 = predict(g1, d, se.fit = F, type = "response")
 d$pred_count_2 = predict(g2, d, se.fit = F, type = "response")
 d$pred_count_3 = predict(g3, d, se.fit = F, type = "response")
-
 d$weight_obs = (d$count-min(d$count))/(max(d$count) - min(d$count))
 d$weight_m1 = (d$pred_count_1 - min(d$pred_count_1))/(max(d$pred_count_1) - min(d$pred_count_1))
 d$weight_m2 = (d$pred_count_2 - min(d$pred_count_2))/(max(d$pred_count_2) - min(d$pred_count_2))
@@ -184,27 +164,19 @@ l_o = wtd.quantile(d$Temperature, q = lq, weight = d$weight_obs, na.rm = T)
 l_1 = wtd.quantile(d$Temperature, q = lq, weight = d$weight_m1, na.rm = T)
 l_2 = wtd.quantile(d$Temperature, q = lq, weight = d$weight_m2, na.rm = T)
 l_3 = wtd.quantile(d$Temperature, q = lq, weight = d$weight_m3, na.rm = T)
-
-t_l = data.frame(t = rbind(l_o, l_1, l_2, l_3), 
-                 g = c("Observation", "Model_A", "Model_B", "Model_C"))
+t_l = data.frame(t = rbind(l_o, l_1, l_2, l_3), g = c("Observation", "Model_A", "Model_B", "Model_C"))
 colnames(t_l) = c("t", "g")
-
 h_o = wtd.quantile(d$Temperature, q = hq, weight = d$weight_obs, na.rm = T)
 h_1 = wtd.quantile(d$Temperature, q = hq, weight = d$weight_m1, na.rm = T)
 h_2 = wtd.quantile(d$Temperature, q = hq, weight = d$weight_m2, na.rm = T)
 h_3 = wtd.quantile(d$Temperature, q = hq, weight = d$weight_m3, na.rm = T)
-
-t_h = data.frame(t = rbind(h_o, h_1, h_2, h_3), 
-                 g = c("Observation", "Model_A", "Model_B", "Model_C"))
+t_h = data.frame(t = rbind(h_o, h_1, h_2, h_3), g = c("Observation", "Model_A", "Model_B", "Model_C"))
 colnames(t_h) = c("t", "g")
-
 T_opt_o = sum(d$weight_obs * d$Temperature/sum(d$weight_obs)) #just weighted mean
 T_opt_1 = sum(d$weight_m1 * d$Temperature/sum(d$weight_m1)) #just weighted mean
 T_opt_2 = sum(d$weight_m2 * d$Temperature/sum(d$weight_m2)) #just weighted mean
 T_opt_3 = sum(d$weight_m3 * d$Temperature/sum(d$weight_m3)) #just weighted mean
-
-t_opt = data.frame(t = rbind(T_opt_o, T_opt_1, T_opt_2, T_opt_3), 
-                   g = c("Observation", "Model_A", "Model_B", "Model_C"))
+t_opt = data.frame(t = rbind(T_opt_o, T_opt_1, T_opt_2, T_opt_3), g = c("Observation", "Model_A", "Model_B", "Model_C"))
 colnames(t_opt) = c("t", "g")
 
 d1 = d[,c("Temperature", "count", "weight_obs")]; d1$g = "Observation"; colnames(d1) = c("t", "n", "w", "g")
@@ -214,7 +186,7 @@ d4 = d[,c("Temperature", "pred_count_3", "weight_m3")]; d4$g = "Model_C"; colnam
 
 d = rbind(d1, d2, d3, d4); rm(d1, d2, d3, d4)
 
-pdf('thermal_percentiles.pdf', height = 5, width = 7)
+pdf('thermal_percentiles.pdf', height = 4, width = 6)
 
 d$g <- factor(d$g, levels = c("Observation", "Model_A", "Model_B", "Model_C"))
 
@@ -231,9 +203,10 @@ d %>%
                               label = paste0("0.05p=", round(t, 1))), hjust = 0, show.legend = FALSE) + 
   geom_text(data = t_h, aes(x = 9, y = c(0.002, 0.0018, 0.0016, 0.0014), 
                               label = paste0("0.95p=", round(t, 1))), hjust = 0, show.legend = FALSE) + 
-  # scale_color_viridis_d("") +
+  scale_color_viridis_d("") +
   ylab("JWS Thermal Occupancy") + xlab("Temperature (deg C)") + 
   theme(legend.position = c(0.75, 0.9))
+
 
 dev.off()
 
@@ -241,5 +214,41 @@ occup = as.data.frame(occup)
 
 save(occup, file = "/Users/Kisei/jws_range/data/occupancy.RData")
 
+d = subset(occup, Bin_width == "0.5 deg C")
+
+q = 90
+
+hq = 1-(1-q/100)/2
+lq = (1-q/100)/2
+
+d = d %>% group_by(Depth_Range) %>% 
+  mutate(count = (count-min(count))/(max(count) - min(count)),
+         l = wtd.quantile(Temperature, q = lq, weight = count, na.rm = T),
+         h = wtd.quantile(Temperature, q = hq, weight = count, na.rm = T),
+         o = sum(count * Temperature/sum(count)))
+
+d$Depth_Range <- factor(d$Depth_Range, levels = c("0-20m", "0-2m"))
+
+setwd("C:/Users/Kisei/Dropbox/PAPER Kisei Bia JWS range shift/figures/figure 2 thermal niche profiling/")
+
+pdf(paste0("Fig.1_", Sys.Date(), ".pdf"), height = 5, width = 5)
+png(paste0("Fig.1_", Sys.Date(), ".png"), height = 5, width = 5, res = 500, units = "in")
+
+d %>% 
+  # subset(Depth_Range == "0-20m") %>% 
+  ggplot(aes(Temperature, count)) + 
+  geom_bar(aes(fill = Depth_Range), stat = "identity", position = "identity", width = 0.5, alpha = 0.8) +
+  # geom_density(aes(fill = Depth_Range, color = Depth_Range), stat = "identity", position = "identity", alpha = 0.1) +
+  geom_segment(data = subset(d, Depth_Range == "0-2m"), aes(x = l, xend = h, y = 1.05, yend = 1.05, color = Depth_Range), size = 1.5, show.legend = F) + 
+  geom_segment(data = subset(d, Depth_Range == "0-20m"), aes(x = l, xend = h, y = 1.06, yend = 1.06, color = Depth_Range), size = 1.5, show.legend = F) + 
+  scale_color_viridis_d("Depth") + 
+  scale_fill_viridis_d("Depth") + 
+  theme_pubr() + 
+  annotate("text", label = "Core Thermal Habitat (90 IQR)", x = 17.96, y = 1.09) + 
+  ylab("Normalized Thermal Occupancy") + xlab("Temperature (deg C)") + 
+  theme(legend.position = c(0.15, 0.9))
+
+dev.off()
+  
 
 
