0107147a51a06f9510bf3dbb0afe0a060773113a
krtanaka <kisei.tanaka@gmail.com>
2020-05-18 13:31:33 -0700

    xfyj

--- scripts/occupancy_map.R
+++ scripts/occupancy_map.R
@@ -4,8 +4,7 @@ library(dplyr)
 library(ggplot2)
 library(raster)
 
-dir = Sys.info()[7]
-load("C:/Users/ktanaka/Dropbox (MBA)/PAPER Kisei Bia JWS range shift/data/t_breadth_2020-05-16.Rdata")
+load("C:/Users/Kisei/jws_range/results/thermal_occupancy.Rdata")
  
 df$month = substr(as.character(df$time), 6, 7)
 df = subset(df, month %in% c("06", "07", "08", "09", "10"))
@@ -23,64 +22,75 @@ df$period = ifelse(df$year %in% c(2012:2016), "2012-2016", df$period)
 df$period = ifelse(df$year %in% c(2017:2019), "2017-2019", df$period)
 
 map = df %>% 
-  group_by(x, y, period) %>% 
-  summarise(p = mean(z)) 
-# mutate(y_mean = sum(z*y)/sum(z))
+  group_by(x, y, period, month) %>% 
+  summarise(p = mean(p, na.rm = T)) 
+
+df %>%
+  group_by(x, y) %>% 
+  summarise(p = mean(z)) %>% 
+  rasterFromXYZ() %>% 
+  area()
 
 xlims = range(map$x); ylims = range(map$y)
-m = map %>% 
+
+m = map %>%
   subset(period != "") %>%
   ggplot(aes(x, y, fill = p)) +
-  geom_tile() +
+  geom_raster() +
   scale_fill_viridis_c("") +
-  borders(xlim = xlims,
-          ylim = ylims,
-          fill = "gray10") +
-  coord_quickmap(xlim = xlims,
-                 ylim = ylims) +
-  facet_wrap(.~ period, nrow = 2) + 
-  theme_void() + 
-  theme(legend.position = "bottom", 
-        legend.justification = c(1, 0.1))
+  # borders(fill = "gray10") +
+  # coord_quickmap(xlim = xlims,
+  #                ylim = ylims) +
+  facet_grid(month~ period)
 
-m
-
-setwd(paste0('/Users/', dir, '/Dropbox/PAPER Kisei Bia JWS range shift/figures/'))
-pdf('thermal_occupancy_map.pdf', height = 5, width = 5)
+setwd('/Users/Kisei/Dropbox/PAPER Kisei Bia JWS range shift/figures/figure 4 total habitat area/')
+pdf('probabilistic_map.pdf', height = 8, width = 10)
 print(m)
 dev.off()
 
 
-load("C:/Users/ktanaka/Dropbox (MBA)/PAPER Kisei Bia JWS range shift/data/t_breadth_2020-05-16.Rdata")
-
-df$month = substr(as.character(df$time), 6, 7)
+load("C:/Users/Kisei/jws_range/results/thermal_occupancy.Rdata")
 
 t = df %>%
   group_by(time) %>%
-  summarise(
-    total = n(),
-    good = sum(z>0, na.rm = T),
-    prop = good/total) %>% 
-  mutate(time = as.Date(time),
-         month = substr(as.character(time), 6, 7)) %>% 
-  mutate(prop = ifelse(month %in% c("06", "07", "08", "09", "10"), prop, NA))
+  summarise(lat_m = sum(p*y)/sum(p)) %>% 
+  mutate(time = as.Date(time))
 
-# lat = df %>% group_by(time) %>% 
-#   # summarise(total_habitat = mean(z, na.rm = T))
-#   mutate(latm = sum(p*y)/sum(p)) %>% 
-#   summarise(thermal_occupancy = mean(latm))
+t$month = substr(as.character(t$time), 6, 7)
+t$year = substr(as.character(t$time), 1, 4)
+
+t1 = t; t1$calender = "Jan-Dec"
+t2 = t; t2$calender = "Jun-Oct"
+
+t2$lat_m = ifelse(t2$month %in% c("06", "07", "08", "09", "10"), t2$lat_m, NA)
+
+t = rbind(t1, t2)
 
 library(zoo)
+library(ggpubr)
 
-ggplot(t, aes(x = time, y = prop, color = prop)) +
-  geom_line(aes(y=rollmean(prop, 10, na.pad = TRUE))) +
-  scale_color_viridis_c("") + 
-  stat_smooth(method = "loess", span = 0.1) +
-  theme_minimal() + 
-  theme(legend.position = "none")
+setwd('/Users/Kisei/Dropbox/PAPER Kisei Bia JWS range shift/figures/figure 4 total habitat area/')
+pdf('habitat_probablistic_a.pdf', height = 4, width = 8)
+ggplot(t, aes(x = time, y = lat_m, color = lat_m)) +
+  geom_line(aes(y = rollmean(lat_m, 10, na.pad = TRUE))) +
+  scale_color_viridis_c("Lat (deg)") + 
+  geom_smooth(method = "loess", span = 0.1) +
+  # geom_smooth(method = "lm", se = F) +
+  ylab("Latitudinal mean of JWS thermal occupancy (deg)") + 
+  ggtitle("10-day running mean") + 
+  theme_classic2() + 
+  facet_wrap(.~calender, ncol = 2, scales = "free_y")
+dev.off()
 
-setwd('/Users/Kisei/Dropbox/PAPER Kisei Bia JWS range shift/figures/')
-pdf('thermal_occupancy_lat_mean.pdf', height = 4, width = 10)
-print(t)
+pdf('habitat_probablistic_b.pdf', height = 4, width = 8)
+ggplot(t, aes(x = time, y = lat_m, color = lat_m)) +
+  # geom_line(aes(y = rollmean(lat_m, 10, na.pad = TRUE))) +
+  scale_color_viridis_c("Lat (deg)") + 
+  geom_smooth(method = "loess", span = 0.1) +
+  geom_smooth(method = "lm", se = F) +
+  ylab("Latitudinal mean of JWS thermal occupancy (deg)") + 
+  ggtitle("10-day running mean") + 
+  theme_classic2() + 
+  facet_wrap(.~calender, ncol = 2, scales = "free_y")
 dev.off()
 
