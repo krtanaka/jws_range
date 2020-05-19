48b05d77b562fd1cb742adc24a42fd324ff869c1
Kisei Tanaka <ktanaka@mbayaq.org>
2020-05-19 14:23:47 -0700

    map fix

--- scripts/calculate_binary.R
+++ scripts/calculate_binary.R
@@ -15,6 +15,8 @@ depth = d
 depth = rasterToPoints(depth)
 depth = as.data.frame(depth)
 
+mode = c("coldtail", "IQR")[2]
+
 r = foreach(year = 1981:2020, .combine = rbind) %dopar% {
   
   # year = 2019
@@ -25,9 +27,8 @@ r = foreach(year = 1981:2020, .combine = rbind) %dopar% {
   
   for (day in 1:dim(df)[3]) {
     
-    # day = 153
-    # day = 180
-    
+    # day = 150
+
     d = df[[day]]
     time = as.character(names(d))
     time = gsub("X", "", time)
@@ -35,22 +36,32 @@ r = foreach(year = 1981:2020, .combine = rbind) %dopar% {
     d = rasterToPoints(d)
     d = as.data.frame(d)
     colnames(d) = c("x", "y", "z")
-    d = merge(d, depth)
-    d$z = ifelse(d$z <= 21.4 & d$z >= 15.3, 1, 0)
+    
+    if (mode == "coldtail") {
+      
+      d = merge(d, depth, all = T)
+      d$z = ifelse(d$z <= 15.6 & d$z >= 14.7, 1, 0) # coldtail = 15.1 +/- 0.4 (centered around 15.1)
+      
+      
+    } else {
+      
+      d = merge(d, depth)
+      d$z = ifelse(d$z <= 21.9 & d$z >= 15.1, 1, 0) # 95 IQR 0.5 temp bin
+      
+    }
+    
     d$time = time
     
     # d %>%
     #   ggplot(aes(x, y, fill = z)) +
     #   geom_tile() +
-    #   # geom_smooth(data = subset(d, z > 0)
-    #   #             , method = "auto",
-    #   #             span = 0.1
-    #   #             ) +
-    #   borders(xlim = range(d$x),
-    #           ylim = range(d$y),
-    #           fill = "gray20") +
+    #   stat_smooth(data = subset(d, z > 0), method = "auto") +
+    #   borders(fill = "gray20") +
     #   coord_quickmap(xlim = range(d$x),
-    #                  ylim = range(d$y))
+    #                  ylim = range(d$y)) + 
+    #   # scale_fill_viridis_c() + 
+    #   theme_void() + 
+    #   theme(legend.position = "none")
     
     year_sum = rbind(year_sum, d)
     
@@ -63,8 +74,5 @@ r = foreach(year = 1981:2020, .combine = rbind) %dopar% {
 }
 
 df = as.data.frame(r)
-save(df, 
-     file = paste0("/Users/ktanaka/Dropbox (MBA)/PAPER Kisei Bia JWS range shift/data/t_breadth_", 
-                   Sys.Date(), 
-                   ".Rdata"))
+save(df, file = paste0("/Users/ktanaka/Dropbox (MBA)/PAPER Kisei Bia JWS range shift/data/t_", mode,".Rdata"))
 
