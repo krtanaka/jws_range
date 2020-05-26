9722b626dd3145a8345bbf1d76d8d585a1b12726
Kisei Tanaka <kisei.tanaka@gmail.com>
2020-05-26 11:50:33 -0700

    kjhj

--- scripts/citizen_science.R
+++ scripts/citizen_science.R
@@ -1,4 +1,4 @@
-setwd("C:/Users/Kisei/Dropbox/PAPER Kisei Bia JWS range shift/data/citizen science")
+setwd("/Users/ktanaka/Dropbox (MBA)/PAPER Kisei Bia JWS range shift/data/citizen science/iNat/")
 
 i <- read_csv("iNat_observs-90879_21May2020.csv")
 # i <- read_csv("observations-79116.csv")
@@ -20,9 +20,10 @@ png('/Users/Kisei/Desktop/Fig.1.draft.png', height = 5, width = 9, units = 'in',
 
 i %>% ggplot(aes(longitude, latitude, color = species_guess)) + 
   scale_color_viridis_d("") + 
-  borders(fill = "gray50") +
-  coord_quickmap(xlim = range(pretty(i$longitude)),
-                 ylim = range(pretty(i$latitude))) +
+  annotation_map(map_data("usa"))+ #Add the map as a base layer before the points
+  # borders(fill = "gray50") +
+  # coord_quickmap(xlim = range(pretty(i$longitude)),
+  #                ylim = range(pretty(i$latitude))) +
   geom_point(size = 5, alpha = 0.5) + 
   scale_x_continuous(breaks = round(seq(min(i$longitude), max(i$latitude), by = 0.5),0)) + 
   facet_wrap(.~ year, scales = "fixed", ncol = 7) +
