ee2376ea7dd452e76646dd50231f5e8419311254
krtanaka <kisei.tanaka@gmail.com>
2020-06-22 16:26:08 -0700

    fsdd

--- scripts/probablistic_maps_timeseries.R
+++ scripts/probablistic_maps_timeseries.R
@@ -9,7 +9,7 @@ library(zoo)
 load("/Users/Kisei/jws_range/data/lat_area.RData")
 load("/Users/ktanaka/jws_range/data/lat_area.RData")
 
-load("/Users/Kisei/Dropbox/PAPER Kisei Bia JWS range shift/data/tags/t_probablistic_lme.Rdata")
+load("/Users/Kisei/Dropbox/PAPER Kisei Bia JWS range shift/data/tags/t_probablistic.Rdata")
 load("/Users/ktanaka/Dropbox (MBA)/PAPER Kisei Bia JWS range shift/data/tags/t_probablistic.Rdata")
 
 df = merge(df, lat_area)
@@ -55,31 +55,31 @@ t0 = df %>%
 
 t1 = df %>% 
   group_by(time) %>% 
-  subset(y >= 22.9 & y <= 33.4) %>%
+  subset(y >= 22.9 & y <= 34.4) %>%
   mutate(area = area * p) %>% 
   summarise(area = sum(area)) %>% 
   mutate(time = as.Date(time),
-         type = "22.9° N - 33.4° N (S.boundary Cali CC LME - Point Conception)")
+         type = "22.9° N - 34.4° N (S.boundary Cali CC LME - Point Conception)")
 
 t2 = df %>% 
   group_by(time) %>% 
-  subset(y >= 33.4 & y <= 37.4) %>%
+  subset(y >= 34.4 & y <= 37.8) %>%
   mutate(area = area * p) %>% 
   summarise(area = sum(area)) %>% 
   mutate(time = as.Date(time),
-         type = "33.4° N - 37.4° N (Point Conception - San Francisco)")
+         type = "34.4° N - 37.8° N (Point Conception - San Francisco)")
 
 t3 = df %>% 
   group_by(time) %>% 
-  subset(y >= 37.4) %>%
+  subset(y >= 37.8) %>%
   mutate(area = area * p) %>% 
   summarise(area = sum(area)) %>% 
   mutate(time = as.Date(time),
-         type = "37.4° N - 47.4° N (San Francisco - N.boundary Cali CC LME)")
+         type = "37.8° N - 47.4° N (San Francisco - N.boundary Cali CC LME)")
 
 t3_missing_days = data.frame(time = setdiff(as.character(t0$time), as.character(t3$time)),
                              area = NA, 
-                             type = "37.4° N - 47.4° N (San Francisco - N.boundary Cali CC LME)")
+                             type = "37.8° N - 47.4° N (San Francisco - N.boundary Cali CC LME)")
 
 t3_missing_days$time = as.Date(t3_missing_days$time)
 
@@ -93,9 +93,9 @@ t = rbind(
 
 t$type <- factor(t$type, levels = c(
   # "22.9° N - 47.4° N (S.boundary Cali CC LME - N.boundary Cali CC LME)",
-  "37.4° N - 47.4° N (San Francisco - N.boundary Cali CC LME)",
-  "33.4° N - 37.4° N (Point Conception - San Francisco)",
-  "22.9° N - 33.4° N (S.boundary Cali CC LME - Point Conception)"))
+  "37.8° N - 47.4° N (San Francisco - N.boundary Cali CC LME)",
+  "34.4° N - 37.8° N (Point Conception - San Francisco)",
+  "22.9° N - 34.4° N (S.boundary Cali CC LME - Point Conception)"))
 
 # p1 = ggplot(t, aes(x = time, y = area, color = area)) +
 #   geom_line(aes(y = rollmean(area, 10, na.pad = TRUE)), alpha = 0.5) +
@@ -108,26 +108,30 @@ t$type <- factor(t$type, levels = c(
 #   theme_pubr(I(20)) + 
 #   theme(legend.position = "none")
    
-p1 = ggplot(t0, aes(x = time, y = area, color = "type", fill = "type")) +
+p1 = ggplot(t0, aes(x = time, y = area/10000, color = "type", fill = "type")) +
   stat_smooth(method = "loess", span = 0.1, aes(color = "type"), show.legend = T) +
-  ylab("JWS Thermal Habitat (sq.km)") + 
-  scale_y_continuous(labels = scientific) + 
+  # scale_y_continuous(labels = scientific) + 
+  # scale_y_continuous(labels = scales::comma) + 
+  ylab(bquote('Available Habitat ( '*10^4~km^2*')')) +  xlab("") + 
   facet_wrap(.~type, scales = "free_y", ncol = 1) +
-  theme_pubr(I(10)) + 
+  theme_few(I(12)) + 
   theme(legend.position = "none")
 
-p2 = ggplot(t, aes(x = time, y = area, color = type, fill = type)) +
+p2 = ggplot(t, aes(x = time, y = area/10000, color = type, fill = type)) +
   scale_fill_viridis_d("") +
   scale_colour_viridis_d("") +
   stat_smooth(method = "loess", span = 0.1, aes(color = type), show.legend = T) +
-  ylab("") + 
-  scale_y_continuous(labels = scientific) + 
+  ylab("") + xlab("") + 
   facet_wrap(.~type, scales = "free_y", ncol = 1) + 
-  theme_pubr(I(10)) + 
+  theme_few(I(12)) + 
   theme(legend.position = "none")
 
 setwd("/Users/Kisei/Desktop")
-png(paste0("Fig.4_binned_lat_timeseries_", Sys.Date(), ".png"), height = 7, width = 10, res = 300, units = 'in')
+# png(paste0("Fig.4_binned_lat_timeseries_", Sys.Date(), ".png"), height = 7, width = 10, res = 300, units = 'in')
+# cowplot::plot_grid(p1, p2, ncol = 2)
+# dev.off()
+
+pdf(paste0("Fig.4_", Sys.Date(), ".pdf"), height = 7, width = 10)
 cowplot::plot_grid(p1, p2, ncol = 2)
 dev.off()
 
