1b8752c17750429dde96dfea9bd2ee7b3effe8df
Kisei Tanaka <kisei.tanaka@gmail.com>
2020-10-13 13:45:22 -1000

    ok

--- scripts/study_area_map.R
+++ scripts/study_area_map.R
@@ -10,17 +10,22 @@ pdf("~/Desktop/S1.pdf", height = 8, width = 8)
 basemap(limits = c(-126, -110, 22.9, 47.4), 
         land.col = "gray20", 
         land.border.col = NA, 
-        bathymetry = TRUE, bathy.style = "poly_greys") + 
-  geom_spatial_point(data = places, aes(x, y, color = label), size = 5) +
+        bathymetry = TRUE) + 
+  # geom_spatial_point(data = places, aes(x, y, color = label), size = 5) +
   scale_color_discrete("") + 
   scale_x_longitude() +
   scale_y_latitude() +
+  annotate(geom = "label", x = -114.5, y = 28, label = "Vizcaíno Bay",  size = 6,
+           fill = alpha(c("white"), 0.8)) + 
+  annotate(geom = "label", x = -120.5, y = 34.4486, label = "Point Conception",  size = 6,
+           fill = alpha(c("white"), 0.8)) +   
+  annotate(geom = "label", x = -121.94, y = 36.8, label = "Monterey Bay",  size = 6,
+           fill = alpha(c("white"), 0.8)) +
   labs(x = "", y = "") + 
   theme_minimal(I(20)) + 
-  theme(legend.position = c(0.75,0.72),
-        legend.title = element_text(color = "white", size = 12),
-        legend.text = element_text(color = "white", size = 12)) + 
-  guides(color=guide_legend(override.aes=list(fill=NA)))
+  theme(legend.position = c(0.75,0.83),
+        legend.title = element_text(color = "white", size = 15),
+        legend.text = element_text(color = "white", size = 15))
 
 dev.off()
   
