e628eead0b41c3282c24f79e443d05df9e7173db
Kisei Tanaka <kisei.tanaka@gmail.com>
2020-10-21 11:44:43 -1000

    crs fixed

--- scripts/calculate_binom_habitat.R
+++ scripts/calculate_binom_habitat.R
@@ -25,7 +25,7 @@ mode = c("coldtail", "IQR")[1]
 
 #add lme
 lme <- readOGR(paste0("/Users/", dir, "/Google Drive/Research/GIS/LME66/LMEs66.shp"))
-CRS.new <- CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0+datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0") #EPSG:102003
+CRS.new <- CRS("+proj=aeqd +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs") #EPSG:102003
 proj4string(lme) <- CRS.new
 
 r = foreach(year = 1981:2020, .combine = rbind, .packages = 'dplyr') %dopar% {
