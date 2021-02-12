c05760e86b6b5fa191c54309078812208f82713d
Kyle Van Houtan <vanhoutan@users.noreply.github.com>
2021-02-12 12:34:24 -0800

    updating directory, data, and code for figure 1b

--- scripts/Fig1c - KVH.R
+++ scripts/Fig1c - KVH.R
@@ -6,11 +6,25 @@
 library(data.table)
 library(viridis)
 library(ggthemes)
+library(ggplot2)
+library(ggthemes)
 
-mbay_jws <- read.csv('~/Dropbox (MBA)/PAPER Kisei Bia JWS range shift/data/community science/JWS_obs_Mbay_2010_2019.csv', header = T)
+themeKV <- theme_few() +
+  theme(strip.background = element_blank(),
+        axis.line = element_blank(),
+        axis.text.x = element_text(margin = margin(0.2, unit = "cm")),
+        axis.text.y = element_text(margin = margin(c(1, 0.2), unit = "cm")),
+        axis.ticks.length=unit(-0.15, "cm"),element_line(colour = "black", size=.5),
+        panel.border = element_rect(colour = "black", fill=NA, size=.5),
+        legend.title=element_blank(),
+        strip.text=element_text(hjust=0))
+
+## load data from within repo
+mbay_jws <- read.csv('./data/JWS_obs_Mbay_2010_2019.csv', header = T)
+
 
 ggplot(data = mbay_jws, aes(x=year, y=rescale)) +
-  theme_few()+
+  themeKV+
   geom_point(aes(color = source)) +
   geom_smooth(method = "loess", formula = y ~ x, span = 0.7, se = TRUE) +
   ## geom_smooth(aes(color = source, fill = source), method = "loess", formula = y ~ x, span = 0.7, se = TRUE) +
