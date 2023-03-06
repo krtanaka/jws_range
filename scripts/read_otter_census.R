f7db78d1728eacb98ac64a89a0d62aae26d62b77
Kisei R. Tanaka <kisei.tanaka@gmail.com>
2023-03-06 13:17:42 -1000

    otter update

--- scripts/read_otter_census.R
+++ scripts/read_otter_census.R
@@ -8,12 +8,15 @@ library(dplyr)
 
 rm(list = ls())
 
-# read sea otter census shapefiles
+# read sea otter census shapefiles, 1985-2019
+# https://www.sciencebase.gov/catalog/item/5601b6dae4b03bc34f5445ec
 shp_list = list.files(path = "data/otter/", pattern = "\\.shp$", full.names = T); shp_list
+
+otter_df_85_19 = NULL
 
 for (shp_i in 1:length(shp_list)) {
   
-  shp_i = 1
+  # shp_i = 1
   
   df <- shapefile(shp_list[shp_i], verbose = T)
   df <- spTransform(df, CRS('+proj=longlat +datum=WGS84'))
@@ -31,25 +34,42 @@ for (shp_i in 1:length(shp_list)) {
   
   otter_df <- as.data.frame(rasterToPoints(ras))
   
-}
+  otter_df_85_19 = rbind(otter_df_85_19, otter_df)
   
+}
 
+otter_df = otter_df_85_19 %>% 
+  group_by(x, y) %>% 
+  summarise(layer = mean(layer))
 
-(p1 = otter_df %>% 
-    ggplot(aes(x, y, fill = layer, color = layer)) +  
+# plot average otter density along CA coast
+
+p1 = otter_df %>% 
+    mutate(x = round(x, 2),
+           y = round(y, 2)) %>% 
+    group_by(x, y) %>% 
+    summarise(layer = mean(layer)) %>% 
+    ggplot(aes(x, y, fill = layer)) +  
     geom_raster(show.legend = T) + 
     coord_equal() + 
-    scale_fill_gradientn(colours = matlab.like(100),"otter_dens_sm", trans = "sqrt") + 
+    scale_fill_gradientn(colours = matlab.like(100),"otter density (m2)", trans = "sqrt") + 
     annotation_map(map = map_data("usa"), fill = "gray20") +
     labs(x = "lon", y = "lat") + 
-    ggdark::dark_theme_minimal())
-
-(p2 = ggplot(data = otter_df, aes(layer, y, fill = layer, color = layer)) +  
+    ggdark::dark_theme_minimal() +  
+    theme(legend.position = c(0.15, 0.2))
+  
+p2 =otter_df %>% 
+    mutate(x = round(x, 2),
+           y = round(y, 2)) %>% 
+    group_by(y) %>% 
+    summarise(layer = mean(layer)) %>% 
+    ggplot(aes(layer, y, fill = layer, color = layer)) +  
     geom_point(shape = 21, show.legend = F) + 
     scale_fill_gradientn(colours = matlab.like(100),"otter_dens_sm", trans = "sqrt") +  
     scale_color_gradientn(colours = matlab.like(100),"otter_dens_sm", trans = "sqrt") + 
     labs(x = "otter_dens_sm", y = "lat") + 
-    ggdark::dark_theme_minimal())
+    coord_fixed(ratio = 12) + 
+    ggdark::dark_theme_minimal()
 
 p1 + p2
 
